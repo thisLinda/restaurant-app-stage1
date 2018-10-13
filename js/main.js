685b5fe47d1dcb0f197e06ecfa791c41ec4cfd23
Linda Zanchi <linda.a.zanchi@gmail.com>
2018-10-13 15:08:29 -0400

    responsiveness needs review

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<pk.eyJ1IjoidGhpc2xpbmRhIiwiYSI6ImNqbXo4bXA5dDB0cTkza3J0Nnkwc3NheWUifQ._k0HW-BKfuaJdwLnhn0kcw>',
+    mapboxToken: 'pk.eyJ1IjoidGhpc2xpbmRhIiwiYSI6ImNqbXo4bXA5dDB0cTkza3J0Nnkwc3NheWUifQ._k0HW-BKfuaJdwLnhn0kcw',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
