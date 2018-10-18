56892a46b1212b9332e37da9481cb1fd83ccce5f
Linda Zanchi <linda.a.zanchi@gmail.com>
2018-10-18 13:43:34 -0400

    CSS and a11y updated

--- js/main.js
+++ js/main.js
@@ -177,6 +177,7 @@ createRestaurantHTML = (restaurant) => {
 
   const more = document.createElement('a');
   more.innerHTML = 'View Details';
+  more.setAttribute("aria-label", "View details about ${restaurant.name)");
   more.href = DBHelper.urlForRestaurant(restaurant);
   more.tabIndex = "3";
   li.append(more)
