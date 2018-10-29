3c36bc292f8f11dfe1638f0001108f7c9a5c8d14
Linda Zanchi <linda.a.zanchi@gmail.com>
2018-10-29 10:47:22 -0400

    corrections following submit 2

--- sw.js
+++ sw.js
@@ -13,13 +13,23 @@ self.addEventListener("install", function (event) {
     event.waitUntil(
         caches.open(staticCacheName).then(function (cache) {
             return cache.addAll([
-                "/",
-                "restaurant.html",
+                "/index.html",
+                "/restaurant.html",
                 "/css/styles.css",
+                "/data/restaurants.json",
+                "/img/1.jpg",
+                "/img/2.jpg",
+                "/img/3.jpg",
+                "/img/4.jpg",
+                "/img/5.jpg",
+                "/img/6.jpg",
+                "/img/7.jpg",
+                "/img/8.jpg",
+                "/img/9.jpg",
+                "/img/10.jpg",
                 "/js/dbhelper.js",
                 "/js/main.js",
-                "js/restaurant_info.js",
-                "data/restaurants.json"
+                "/js/restaurant_info.js",
             ]);
         })
     );
